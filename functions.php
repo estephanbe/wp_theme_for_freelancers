e16a02bd19ea56d28f47f4d01d677d149dd4a41e
Bisharah Estephan <estephan.be@gmail.com>
2023-04-29 16:09:39 +0300

    adding menu

--- functions.php
+++ functions.php
@@ -1,13 +1,17 @@
 <?php
 
-function theme_support()
-{
-    add_theme_support('title-tag');
-}
-add_action('after_setup_theme', 'theme_support');
-
 function add_theme_scripts()
 {
     wp_enqueue_style('main-style', get_stylesheet_uri());
 }
 add_action('wp_enqueue_scripts', 'add_theme_scripts');
+
+function register_my_menus()
+{
+    register_nav_menus(
+        array(
+            'header-menu' => __('Header Menu')
+        )
+    );
+}
+add_action('init', 'register_my_menus');
